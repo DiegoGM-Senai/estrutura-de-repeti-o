3cd1f06dfa767ba70ad882339f9bed6611a450d5
Diego Garcia Marques <Diego_G-Marques@estudante.sesisenai.org.br>
2021-11-24 17:26:51 -0300

    primeiro

---
+++ array.js
@@ -0,0 +1,6 @@
+const diasDaSemana = ["domingo","segunda","terca","quarta"]
+
+console.log(diasDaSemana[0]);
+console.log(diasDaSemana[1]);
+console.log(diasDaSemana[2]);
+console.log(diasDaSemana[3]);
